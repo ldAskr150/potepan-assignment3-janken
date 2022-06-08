27ccf8cc24c1c2980833b6f5c3c9ce8dd0bd4b76
EC2 Default User <ec2-user@ip-172-31-34-183.us-west-2.compute.internal>
2022-06-08 13:18:20 +0000

    課題修正

--- janken.rb
+++ janken.rb
@@ -1,63 +1,68 @@
-puts "じゃんけん…"
+class JankenAcchi
+  attr_accessor :player_hand
   
-def janken
-    
-  puts "[0]グー\n[1]チョキ\n[2]パー"
-    
-  player_hand = gets.to_i
-  program_hand = rand(3)
-    
-  jankens = ["グー", "チョキ", "パー"]
-  puts "ホイ！"
-  puts "あなたの手:#{jankens[player_hand]}, 相手の手:#{jankens[program_hand]}"
-    
-  if player_hand == program_hand
-    puts "あいこで…"
-    return true
-  
-  elsif (player_hand == 0 && program_hand== 1)||(player_hand == 1 && program_hand == 2)||(player_hand == 2 && program_hand == 0)
-    puts "じゃんけんに勝った！"
-    puts "------------------------"
-    return false
-        
-  else
-    puts "じゃんけんに負けた…"
-    puts "------------------------"
-    return false
+  def setup
+    @player_hand = player_hand
   end
+  
+  def janken
+    puts "[0]グー\n[1]チョキ\n[2]パー"
+    @player_hand = gets.to_i
+    program_hand = rand(3)
+      
+    jankens = ["グー", "チョキ", "パー"]
+    puts "ホイ！"
+    puts "あなたの手:#{jankens[@player_hand]}, 相手の手:#{jankens[program_hand]}"
+      
+    if @player_hand == program_hand
+      puts "あいこで…"
+      return true
     
+    elsif (@player_hand == 0 && program_hand== 1)||(@player_hand == 1 && program_hand == 2)||(@player_hand == 2 && program_hand == 0)
+      puts "じゃんけんに勝った！"
+      puts "------------------------"
+      return false
+          
+    else
+      puts "じゃんけんに負けた…"
+      puts "------------------------"
+      return false
+    end
+  end
+  
+  def acchimuite
+    puts "[0]上\n[1]下\n[2]左\n[3]右"
+    player_face = gets.to_i
+    program_face = rand(4)
+      
+    directions = ["上", "下", "左", "右"]
+    puts "ホイ！"
+    puts "あなたの向き:#{directions[player_face]}, 相手の向き:#{directions[program_face]}"
+      
+    if player_face == program_face
+      if (@player_hand == 0 && program_hand== 1)||(@player_hand == 1 && program_hand == 2)||(@player_hand == 2 && program_hand == 0)
+        puts "あっち向いてホイに勝った！"
+      else
+        puts "あっち向いてホイに負けた"
+      end
+        
+    else
+      puts "引き分け"
+    end
+  end
 end
+
+
+puts "じゃんけん…"
+hoge = JankenAcchi.new
+hoge.janken
 
 next_game = true
-  
 while next_game
-  next_game = janken
+  next_game = hoge.janken
 end
-  
-  
-puts "あっち向いて…"
 
-def acchimuite
-  puts "[0]上\n[1]下\n[2]左\n[3]右"
-    
-  player_face = gets.to_i
-  program_face = rand(4)
-    
-  directions = ["上", "下", "左", "右"]
-  puts "ホイ！"
-  puts "あなたの向き:#{directions[player_face]}, 相手の向き:#{directions[program_face]}"
-    
-    
-  if player_face == program_face
-    if (player_hand == 0 && program_hand== 1)||(player_hand == 1 && program_hand == 2)||(player_hand == 2 && program_hand == 0)
-      puts "あっち向いてホイに勝った！"
-    else
-      puts "あっち向いてホイに負けた"
-    end
-      
-  else
-    puts "引き分け"
-      
-  end
-  
-end
+puts "あっち向いて…"
+hoge.acchimuite
+puts "ゲーム終了"
+puts "------------------------"
